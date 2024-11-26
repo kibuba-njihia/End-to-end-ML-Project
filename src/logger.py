e9dced1f868dbcba0b2ed2bf0c6284c793ef8a1f
Kibuba <kibubanjihia@gmail.com>
2024-11-26 12:39:26 +0300

    Updating project structure: Logger and exception

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now.strftime('%m_%d_%Y_%H_%M_%S_')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True) # Even tho there are file in this folder, keep appending file to the folder
 
@@ -12,5 +12,5 @@ LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
 )
